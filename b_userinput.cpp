d5238bea196f6e929e6ff74bb1176a2171fdda8e
Bhavya Paliwal <anubhapaliwal955@gmail.com>
2026-03-26 08:38:35 +0530

    Add files via upload

---
+++ b_userinput.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num ;
+    char ch ;
+    string name;
+    cout << "Enter a number " << endl;
+    cin >> num; 
+    cout<<"the number is "<< num << endl ;
+
+
+    cout << "Enter a character " << endl;
+    cin >> ch; 
+    cout<<"the character is "<< ch << endl ;
+
+
+    cout << "Enter your name " << endl;
+    cin >> name; 
+    cout<<"My name is "<< name << endl ;
+
+return 0 ;
+}
